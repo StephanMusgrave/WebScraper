1a3a9dafb5766dea24856cf8f7aadb04c5007905
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-11-18 18:40:35 +0000

    puts page.css('td[style=text-align:left

--- wikipedia_scraper.rb
+++ wikipedia_scraper.rb
@@ -3,4 +3,4 @@ require 'nokogiri'
 
 url = "http://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
 page = Nokogiri::HTML(open(url))
-puts page.css('li.toclevel-3')
+puts page.css('td[style="text-align:left;"]') 
